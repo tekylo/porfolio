336c213a5aa412f3080ddf5bb80588b1d70e199c
tekylo <tekylo@gmail.com>
2019-09-16 23:33:51 +0200

    Fix package

---
+++ precache-manifest.53e77479402a47cfe5981f08a33d1150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31181954e4a577b9169fceb28b060fce",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "aaf962616898af4aeb1c",
+    "url": "/porfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "704f518e4a2ae3b0e930",
+    "url": "/porfolio/static/js/2.428394aa.chunk.js"
+  },
+  {
+    "revision": "aaf962616898af4aeb1c",
+    "url": "/porfolio/static/js/main.75aaece7.chunk.js"
+  },
+  {
+    "revision": "8df1f7692abb9579f62f",
+    "url": "/porfolio/static/js/runtime~main.f0bd08e5.js"
+  }
+]);
