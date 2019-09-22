9a02759cacc602e8b5f602abe30678f942ba80b6
Jose Antonio Lopez Molina <tekylo@gmail.com>
2019-09-22 19:46:27 +0200

    Updates

---
+++ precache-manifest.8ab82cc7aaa7d0facfc0a0f522705462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5135cbed8badca411cd72781dbae3333",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "de9ae7c7c45198f6197f",
+    "url": "/porfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "704f518e4a2ae3b0e930",
+    "url": "/porfolio/static/js/2.428394aa.chunk.js"
+  },
+  {
+    "revision": "de9ae7c7c45198f6197f",
+    "url": "/porfolio/static/js/main.fdc1ab12.chunk.js"
+  },
+  {
+    "revision": "8df1f7692abb9579f62f",
+    "url": "/porfolio/static/js/runtime~main.f0bd08e5.js"
+  }
+]);
